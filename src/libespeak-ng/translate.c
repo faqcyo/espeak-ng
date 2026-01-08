de752a11c50e63a359f5ed6c52919f3141206e9e
Facu <cayofacundo1@gmail.com>
2026-01-08 20:28:09 -0300

    testing other values of clause_pause on abbreviation

--- src/libespeak-ng/translate.c
+++ src/libespeak-ng/translate.c
@@ -1620,7 +1620,7 @@ void TranslateClauseWithTerminator(Translator *tr, int *tone_out, char **voice_c
 
 			if ((dict_flags & (FLAG_ALLOW_DOT | FLAG_NEEDS_DOT)) && (ix == word_count - 1 - dictionary_skipwords) && (terminator & CLAUSE_DOT_AFTER_LAST_WORD)) {
 				// probably an abbreviation such as Mr. or B. rather than end of sentence
-				clause_pause = 10;
+				clause_pause = 1;
         printf("Abbreviation");
 				if (tone_out != NULL)
 					*tone_out = 4;
