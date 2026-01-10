d6ef483ba4d61b36693320e4f5a18250c98932b8
Facu <cayofacundo1@gmail.com>
2026-01-10 05:54:47 -0300

    more debug prints

--- src/libespeak-ng/translate.c
+++ src/libespeak-ng/translate.c
@@ -1620,8 +1620,7 @@ void TranslateClauseWithTerminator(Translator *tr, int *tone_out, char **voice_c
 
 			if ((dict_flags & (FLAG_ALLOW_DOT | FLAG_NEEDS_DOT)) && (ix == word_count - 1 - dictionary_skipwords) && (terminator & CLAUSE_DOT_AFTER_LAST_WORD)) {
 				// probably an abbreviation such as Mr. or B. rather than end of sentence
-				clause_pause = 0;
-        printf("Abbreviation");
+				clause_pause = 10;
 				if (tone_out != NULL)
 					*tone_out = 4;
 			}
@@ -1676,6 +1675,7 @@ void TranslateClauseWithTerminator(Translator *tr, int *tone_out, char **voice_c
 		else
 			*voice_change = NULL;
 	}
+  fprintf(stderr, "[DEBUG] is_abbreviation: %s, word_flags: %s\n", (words[word_count-1].flags & FLAG_HAS_DOT) == FLAG_HAS_DOT, words[word_count-1]);
 }
 
 void TranslateClause(Translator *tr, int *tone_out, char **voice_change)
